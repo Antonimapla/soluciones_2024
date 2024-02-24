7f4087133a504457766c4e6f40e80f6f69fd0735
Antonimapla <tonimapla@gmail.com>
2024-02-24 22:46:40 +0100

    pilas y colas

---
+++ 07_colas.py
@@ -0,0 +1,16 @@
+### colas ###
+
+cola = []
+cola.append("primero")
+cola.append("segundo")
+cola.append("tercero")
+
+print("Cola inicial:")
+print(cola)
+
+print("\nRetirando de la cola: ")
+print(cola.pop(0))
+print(cola.pop(0))
+print(cola.pop(0))
+print("\nLa cola despues de retirar los elementos: ")
+print(cola)
---
+++ 07_pilas.py
@@ -0,0 +1,15 @@
+# pilas 
+
+my_stack = []
+my_stack.append('primero')
+my_stack.append('segundo')
+my_stack.append('tercero')
+my_stack.append('cuarto')
+
+print(my_stack)
+
+my_stack.pop()
+print(my_stack)
+
+my_stack.pop()
+print(my_stack)
