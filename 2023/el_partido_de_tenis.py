214a1befd1759cc0ea375339da16b123db7de6fe
Antonimapla <tonimapla@gmail.com>
2024-03-04 23:50:06 +0100

    tenis

--- 2023/el_partido_de_tenis.py
+++ 2023/el_partido_de_tenis.py
@@ -1,3 +1,5 @@
+
+
 """
 .Escribe un programa que muestre cómo transcurre un juego de tenis y quién lo ha ganado.
  * El programa recibirá una secuencia formada por "P1" (Player 1) o "P2" (Player 2), según quien
@@ -16,33 +18,92 @@
  * - Consulta las reglas del juego si tienes dudas sobre el sistema de puntos.
 """
 
-puntos = []
+puntos = 0
 puntos_P1 = 0
 puntos_P2 = 0
 juego = 0
 pset = 0
-ganador = ""
+#ganador = 
 puntos1 = ["love", "15", "30", "40", "deuce", "ventaja", "winner"]
 puntos2 = ["love", "15", "30", "40", "deuce", "ventaja", "winner"]
 p1 = 0
 p2 = 0
-
-def ganador_pset():
-    ganador = input("Introduzca jugador ganador p1 / p2: ")
-    return ganador
-
-p1p2 = ganador_pset()
-
-def marcador(p1p2):
-    if p1p2 == p1:
-        pass
+bola = True
+marca1 = ""
+marca2 = ""
 
 
+while bola == True:
 
+    #def ganador_pset():
+    ganador = input("Introduzca jugador ganador p1 - p2: ")
+        #return ganador
+    
+    p1p2 = ganador
 
-
+    def reset_contadores():
+        puntos_P1 = 0
+        puntos_P2 = 0
 
     
-def puntos_jugadores(p1p2):
-    pass
+    
+    def progreso_juego(p1p2):
+        if p1p2 == p1:
+            puntos_P1 += 1
+            marcador(puntos_P1)
+
+        elif p1p2 == p2:
+            puntos_P2 += 1
+            marcador(puntos_P2)
+
+    progreso_juego(p1p2)
+
+    def marcador(p1p2):
+        puntos1 = ["love", "15", "30", "40", "deuce", "ventaja", "winner"]
+        puntos2 = ["love", "15", "30", "40", "deuce", "ventaja", "winner"]
+
+        if puntos_P1 == 1:
+            marca1 = "love"
+        elif puntos_P1 == 2:
+            marca1 = "15"
+        elif puntos_P1 == 3:
+            marca1 = "30"
+        elif puntos_P1 == 4:
+            marca1 = "40"
+        elif puntos_P1 == 5:
+            marca1 = "deuce"
+        elif puntos_P1 == 6:
+            marca1 = "ventaja"
+        elif puntos_P1 == 7:
+            marca1 = "winner"
+        else: 
+            if puntos_P2 == 1:
+                marca2 = "love"
+            elif puntos_P2 == 2:
+                marca2 = "15"
+            elif puntos_P2 == 3:
+                marca2= "30"
+            elif puntos_P2 == 4:
+                marca2 = "40"
+            elif puntos_P2 == 5:
+                marca2 = "deuce"
+            elif puntos_P2 == 6:
+                marca2 = "ventaja"
+            elif puntos_P2 == 7:
+                marca2 = "winner"
+
+        print(f"Resultado provisional del partido jugador1 {marca1}")
+        print(f"Resultado provisional del partido jugador2 {marca2}")
+
+    
+    if marca1 or marca2== "winner":
+       reset_contadores()
+       bola = False        
+
+
+
+
+
+        
+    
     
