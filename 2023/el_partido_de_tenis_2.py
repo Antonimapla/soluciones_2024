2689ef8fe4eb33c5e9cb4516f13c4aa1d5b82278
Antonimapla <tonimapla@gmail.com>
2024-03-10 22:14:11 +0100

    fin de tenis

--- 2023/el_partido_de_tenis_2.py
+++ 2023/el_partido_de_tenis_2.py
@@ -24,24 +24,26 @@ jugador_2 = 0
 dato = 0
 ganador = ""
 dato = 1    
-p1 = ""
-p2 = ""
+p1 = "p1"
+p2 = "p2"
 marca1 = 0
 marca2 = 0
-global jugada 
+jugada = 0
             
 
 def puntuacion(jugador):
+    global jugador_1, jugador_2
     if jugador == p1:
-        jugador_1 + 1
-        marcador1(jugador_1)
+        jugador_1 += 1
+        marcador1(jugador_1, jugador_2)
     else:
-        jugador_2 = jugador_2 + 1
-        marcador2(jugador_2)
+        jugador_2 +=  1
+        marcador2(jugador_2, jugador_1)
 
 
 
 def diccio(jugada, ganador):
+    global p1, p2
     puntos[jugada] = ganador 
     if ganador == p1:
         puntuacion(p1)
@@ -51,56 +53,72 @@ def diccio(jugada, ganador):
 
         
 def entrada_datos(jugada):
-    jugada = jugada + 1
+    global p1, p2
+    jugada += 1
     ganador = input(f"Introduzca jugador ganador (p1 o p2) de la jugada {jugada}: ")
-    print(jugada)
     diccio(jugada, ganador)
-print(jugador_1)
+    return jugada 
 
-def marcador1(jugador_1):
+def marcador1(jugador_1, jugador_2):
+    global marca1
     puntos_P1 = jugador_1
-    if puntos_P1 == 1:
+    if puntos_P1 == 0:
         marca1 = "love"
-    elif puntos_P1 == 2:
+    elif puntos_P1 == 1:
         marca1 = "15"
-    elif puntos_P1 == 3:
+    elif puntos_P1 == 2:
         marca1 = "30"
-    elif puntos_P1 == 4:
+    elif puntos_P1 == 3:
         marca1 = "40"
-    elif puntos_P1 == 5:
+    elif puntos_P1 == 4:
         marca1 = "deuce"
-    elif puntos_P1 == 6:
+    elif puntos_P1 == 5:
         marca1 = "ventaja"
-    elif puntos_P1 == 7:
+    elif puntos_P1 == 6:
         marca1 = "winner"
 
-print(f"jugador 1: {marca1}")        
+    #print(f"jugador 1: {marca1}")        
+    imprimir_puntuacion(marca1, marca2)
+    if marca1 == "winner":
+        exit()
 
-def marcador2(jugador_2):
+def marcador2(jugador_2, jugador_1):
+    global marca2
     puntos_P2 = jugador_2
-    if puntos_P2 == 1:
+    if puntos_P2 == 0:
         marca2 = "love"
-    elif puntos_P2 == 2:
+    elif puntos_P2 == 1:
         marca2 = "15"
-    elif puntos_P2 == 3:
+    elif puntos_P2 == 2:
         marca2= "30"
-    elif puntos_P2 == 4:
+    elif puntos_P2 == 3:
         marca2 = "40"
-    elif puntos_P2 == 5:
+    elif puntos_P2 == 4:
         marca2 = "deuce"
-    elif puntos_P2 == 6:
+    elif puntos_P2 == 5:
         marca2 = "ventaja"
-    elif puntos_P2 == 7:
+    elif puntos_P2 == 6:
         marca2 = "winner"
+        
+    imprimir_puntuacion(marca1, marca2)
+    #print(f"jugador 2: {marca2}")
+    if marca2 == "winner":
+        exit()
 
-print(f"jugador 2: {marca2}")
+def imprimir_puntuacion(marca1, marca2):
+    print('\n')
+    print(f"JUGADA {jugada + 1}   Jugador 1 : {marca1}    Jugador 2 : {marca2}")    
+
 
 x = 16
-jugada = 0      
+      
 while x > 0:
-    entrada_datos(jugada + 1)
+    jugada = entrada_datos(jugada)
     x -= 1
+     
+        #print("Fin del juego")
 
+print(puntos)
     
     
     
